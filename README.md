1917d149c5e294dbc59dacc2e982fc2d7d59d89f
hellopsm <psm2000163@163.com>
2023-01-04 19:37:51 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-tubiao
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-tz3ukq)
---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <lay-icon type="layui-icon-face-smile"></lay-icon> &nbsp;
+  <lay-icon type="layui-icon-face-smile" color="#009688"></lay-icon> &nbsp;
+  <lay-icon type="layui-icon-face-smile" color="#5FB878"></lay-icon> &nbsp;
+  <lay-icon type="layui-icon-face-smile" color="#1E9FFF"></lay-icon> &nbsp;
+  <lay-icon type="layui-icon-face-smile" color="#FFB800"></lay-icon> &nbsp;
+  <lay-icon type="layui-icon-face-smile" color="#FF5722" size="24px"></lay-icon> &nbsp;
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+
+    return {
+    }
+  }
+}
+</script>
